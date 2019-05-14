847159296042b5a21a3de507a3bc16a602d08c4c
Chulwoo <chulwoojkim@gmail.com>
2019-05-14 14:58:20 -0400

    Working version of Binary Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
+//This class contains a method that implements binary search.
+//Binary search is a searching algorithm that looks in an ordered
+//array for the element given.
+//Written by: Chulwoo Kim
 public class BinarySearch{
-    int[] testArray;
 
+    //Creates an int[] and checks if a number
+    //given is in the  array.
     public static void main(String[] args){
 	Scanner s = new Scanner(System.in);
 	int[] intArray = new int[]{1,2,3,4,5,6,7,9,10,11,12,14,15};
@@ -10,7 +15,8 @@ public class BinarySearch{
 	System.out.println(binarySearch(intArray, hold));
     }
 
-
+    //Goes through the int[] and returns true if the int[] contains
+    //the integer. Otherwise it returns false;
     public static boolean binarySearch(int[] data, int toFind){
 	int left = 0;
 	int right = data.length -1;
